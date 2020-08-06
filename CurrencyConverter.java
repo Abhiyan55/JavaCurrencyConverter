28148145101d1f799d57e38f16721bad7705d2b8
Abhiyan55 <abhirimal009@gmail.com>
2020-08-06 22:08:06 +0545

    Currency Converter - Version 1.3

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,34 +1,59 @@
-// Currency Converter Version 1.2
 
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*; 
 
- 	class CurrencyConverter extends JFrame {
+ 	public class CurrencyConverter implements ActionListener {
 
 		public static void main(String[] args){
-			CurrencyConverter g=new CurrencyConverter();
+			new CurrencyConverter();
+		}
+		
+		JFrame f;
+		JButton b1;
+		JTextField t1;
+		JLabel l1,l2,l3,l4;
+
+	CurrencyConverter() {
+		
+		f= new JFrame();
+		f.setSize(450,350);
+		f.setBounds(450,300, 450, 300);
+		
+		l1 = new JLabel("CURRENCY CONVERTER");
+		l1.setBounds(135, 10, 150,50);
+		l2 = new JLabel("Enter amount in USD");
+		l2.setBounds(40, 50, 250, 74);
+		t1 = new JTextField(4);
+		t1.setBounds(163, 78, 35, 20);
+		b1 = new JButton("Convert");
+		b1.setBounds(163, 142, 89, 23);
+		l3 = new JLabel("Rs.");	
+		l3.setBounds(163, 182, 89, 23);
+		l4 = new JLabel("--");
+		l4.setBounds(186, 182, 89, 23);
+		
+		f.add(l1);
+		f.add(l2);
+		f.add(t1);
+		f.add(b1);
+		f.add(l3);
+		f.add(l4);
+
+		b1.addActionListener(this);  //Action listener is an interface
+		
+		f.setResizable(false); // disables full screen
+		f.setLayout(null);
+		f.setVisible(true);   
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);                  
 		}
 
-	JButton b1;
-	CurrencyConverter(){
-		setSize(500,500);
-		
-		JLabel l = new JLabel("Enter amount in NRS (Rs.)");
-		JTextField t1 = new JTextField(4);
-		b1=new JButton("Convert to USD");	
-		
-		
-		add(l);
-		add(t1);
-		add(b1);
-		
-
-		setLayout(new FlowLayout());
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);                       
-
+	
+		public void actionPerformed(ActionEvent ae) {	
+		int num1=Integer.parseInt(t1.getText());
+		int value = num1 * 120;
+		l4.setText(value + "" );	
 		}
-
 		
 	}
