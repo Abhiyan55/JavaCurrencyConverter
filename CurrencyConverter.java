60c10a784014c6bf7438b35476dad3463aba1855
Abhiyan55 <abhirimal009@gmail.com>
2020-08-08 15:03:48 +0545

    Currency Converter - Version 1.6

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,4 +1,5 @@
 
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*; 
@@ -12,7 +13,7 @@ import java.awt.event.*;
 		JFrame f;
 		JButton b1,b2;
 		JTextField t1;
-		JLabel l1,l2,l3,l4,l5,l6,l7;
+		JLabel l1,l2,l3,l4,l5,l6,l7,l8;
 		JComboBox c1,c2;
 		double value=0.0;
 		String[] currencies1 = {"USD", "NRS", "EUR"};
@@ -22,32 +23,34 @@ import java.awt.event.*;
 		
 		f= new JFrame();
 		f.setSize(450,350);
-		f.setBounds(450,300, 450, 300);
+		f.setBounds(450,250,500,350);
 		
 		l1 = new JLabel("CURRENCY CONVERTER");
-		l1.setBounds(135, 0, 150,50);
+		l1.setBounds(165, 0, 150, 50);
 		l2 = new JLabel("Enter amount");
-		l2.setBounds(145, 100, 250, 74);
+		l2.setBounds(175, 120, 250, 74);
 		t1 = new JTextField("0");
-		t1.setBounds(230, 125, 55, 20);
+		t1.setBounds(260, 145, 55, 20);
 		b1 = new JButton("Convert");
-		b1.setBounds(123, 165, 89, 23);
+		b1.setBounds(153, 200, 89, 23);
 		l3 = new JLabel("Amount:");	
-		l3.setBounds(173, 195, 89, 23);
+		l3.setBounds(213, 240, 89, 23);
 		l4 = new JLabel("--");
-		l4.setBounds(226, 195, 200, 23);
+		l4.setBounds(266, 240, 200, 23);
 		c1 = new JComboBox(currencies1);
-		c1.setBounds(120, 82, 65, 28);
+		c1.setBounds(150, 92, 65, 28);
 		c2 = new JComboBox(currencies2);
-		c2.setBounds(270, 82, 65,28);
+		c2.setBounds(300, 92, 65,28);
 		l5 = new JLabel(" (Select Your Prefered Currency) ");
-		l5.setBounds(115, 35, 250,50);
+		l5.setBounds(135, 45, 250,50);
 		l6 = new JLabel("FROM:");
-		l6.setBounds(70, 82, 65, 28);
+		l6.setBounds(100, 92, 65, 28);
 		l7 = new JLabel("TO:");
-		l7.setBounds(240, 82, 65, 28);
+		l7.setBounds(270, 92, 65, 28);
 		b2 = new JButton("Reset");
-		b2.setBounds(240, 165, 89, 23);
+		b2.setBounds(270, 200, 89, 23);
+		l8 = new JLabel();
+		l8.setBounds(148, 263, 280, 23);
 		
 		f.add(l1);
 		f.add(l2);
@@ -61,17 +64,27 @@ import java.awt.event.*;
 		f.add(l6);
 		f.add(l7);
 		f.add(b2);
+		f.add(l8);
 		
 		b1.addActionListener(this); 
+		b2.addActionListener(this);
+		b1.setActionCommand("Convert");
+		b2.setActionCommand("Reset");
 		
-		f.setResizable(false); // disables full screen
+		f.setResizable(false);			 // disables full screen
 		f.setLayout(null);
 		f.setVisible(true);   
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);                  
 		}
 
 	
-		public void actionPerformed(ActionEvent ae) {	
+		public void actionPerformed(ActionEvent ae) {		
+		
+			String m=ae.getActionCommand();
+			
+			if(m.equals("Convert")) {          //Condition for Convert
+
+
 			double num1=Double.parseDouble(t1.getText());
 			
 
@@ -119,23 +132,33 @@ import java.awt.event.*;
 			
 			if(c1.getSelectedItem().equals("USD")&& c2.getSelectedItem().equals("USD")) {
 
-				l4.setText( " Please select different currency " );	
+				l8.setText( " Please select different currency. " );	
 			}
 
 			if(c1.getSelectedItem().equals("EUR")&& c2.getSelectedItem().equals("EUR")) {
 
-				l4.setText( " Please select different currency "  );	
+				l8.setText( " Please select different currency. "  );	
 			}
 
 			if(c1.getSelectedItem().equals("NRS")&& c2.getSelectedItem().equals("NRS")) {
 
-			l4.setText( " Please select different currency " );	
+			l8.setText( " Please select different currency. " );	
 			}
-			
-			
+		}
+
 
 			
+		if(m.equals("Reset"))   //condition for Reset
 
+
+		{
+		t1.setText("0");
+    		l4.setText("--");
+		l8.setText("");
+		}
+
+		
+			
 
 		}	
 		
